98294eb9263dd0124bc9beb98dbe0725e631b307
dashodanger <none@none.com>
2022-06-22 14:36:19 -0600

    Update miniz

--- source_files/stb/stb_image_write.h
+++ source_files/stb/stb_image_write.h
@@ -809,6 +809,27 @@ STBIWDEF int stbi_write_hdr(char const *filename, int x, int y, int comp, const 
 // PNG writer
 //
 
+#include "../miniz/miniz.h"
+unsigned char* compress_for_stbiw(unsigned char *data, int data_len,
+                                  int *out_len, int quality)
+{
+  uLongf bufSize = compressBound(data_len);
+  // note that buf will be free'd by stb_image_write.h
+  // with STBIW_FREE() (plain free() by default)
+  unsigned char* buf = (unsigned char *)malloc(bufSize);
+  if(buf == NULL)  return NULL;
+  if(compress2(buf, &bufSize, data, data_len, quality) != Z_OK)
+  {
+    free(buf);
+    return NULL;
+  }
+  *out_len = bufSize;
+
+  return buf;
+}
+
+#define STBIW_ZLIB_COMPRESS compress_for_stbiw
+
 #ifndef STBIW_ZLIB_COMPRESS
 // stretchy buffer; stbiw__sbpush() == vector<>::push_back() -- stbiw__sbcount() == vector<>::size()
 #define stbiw__sbraw(a) ((int *) (void *) (a) - 2)

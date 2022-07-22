253b1df1e1fb98aa91c96e4c6b26213db6c81928
dashodanger <none@none.com>
2022-07-22 17:21:08 -0600

    A more generic way to handle the blank episode issue

--- source_files/edge/m_menu.cc
+++ source_files/edge/m_menu.cc
@@ -1133,8 +1133,7 @@ static void CreateEpisodeMenu(void)
 		gamedef_c *g = ITERATOR_TO_TYPE(it, gamedef_c*);
 		if (! g) continue;
 
-		// Not sure why, but sometimes the built-in "EDGE" gamedef gets treated as a valid episode
-		if (strcasecmp(g->name.c_str(), "EDGE") == 0)
+		if (g->firstmap.empty())
 			continue;
 
 		if (W_CheckNumForName(g->firstmap.c_str()) == -1)

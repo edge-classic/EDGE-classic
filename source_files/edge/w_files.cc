014dca02a80e0a23da69ce58a0ac4e69b226590f
Andrew Apted <ajapted@gmail.com>
2022-09-13 20:00:44 +1000

    pk3 : added "w_files.cc" and "w_files.h" code (mostly empty).

---
+++ source_files/edge/w_files.cc
@@ -0,0 +1,37 @@
+//----------------------------------------------------------------------------
+//  EDGE file handling
+//----------------------------------------------------------------------------
+//
+//  Copyright (c) 2022  The EDGE Team.
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+//
+//  Based on the DOOM source code, released by Id Software under the
+//  following copyright:
+//
+//    Copyright (C) 1993-1996 by id Software, Inc.
+//
+//----------------------------------------------------------------------------
+
+#include "i_defs.h"
+
+#include <list>
+#include <vector>
+#include <algorithm>
+
+#include "w_files.h"
+
+// TODO
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab
---
+++ source_files/edge/w_files.h
@@ -0,0 +1,39 @@
+//----------------------------------------------------------------------------
+//  EDGE file handling
+//----------------------------------------------------------------------------
+//
+//  Copyright (c) 2022  The EDGE Team.
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+//
+//  Based on the DOOM source code, released by Id Software under the
+//  following copyright:
+//
+//    Copyright (C) 1993-1996 by id Software, Inc.
+//
+//----------------------------------------------------------------------------
+
+#ifndef __W_FILES__
+#define __W_FILES__
+
+#include "dm_defs.h"
+
+#include "file.h"
+#include "utility.h"
+
+// TODO
+
+#endif // __W_FILES__
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab

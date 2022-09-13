53741f5c2ef09f42e6f279db69812eb48eaf9e4b
Andrew Apted <ajapted@gmail.com>
2022-09-13 19:02:38 +1000

    added (mostly empty) "w_pk3.cc" and "w_pk3.h" code files.

---
+++ source_files/edge/w_pk3.cc
@@ -0,0 +1,30 @@
+//----------------------------------------------------------------------------
+//  EDGE PK3 Support Code
+//----------------------------------------------------------------------------
+//
+//  Copyright (c) 2022  The EDGE Team.
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+
+#include "i_defs.h"
+
+#include <list>
+#include <vector>
+#include <algorithm>
+
+#include "miniz.h"
+
+// TODO
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab

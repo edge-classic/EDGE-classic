0d40db5db150a57229a5fb764e30b06a1e0e4747
Andrew Apted <ajapted@gmail.com>
2022-10-22 13:08:00 +1100

    bot : added empty code files "p_navigate.cc" and "p_navigate.h".
    This will contain functions to allow a bot to navigate a map, such as
    how to get from point A and point B, and how to avoid obstacles.

---
+++ source_files/edge/p_navigate.cc
@@ -0,0 +1,35 @@
+//----------------------------------------------------------------------------
+//  EDGE Navigation System
+//----------------------------------------------------------------------------
+//
+//  Copyright (c) 2022  The EDGE Team.
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+
+#include "i_defs.h"
+
+#include "con_main.h"
+#include "dm_defs.h"
+#include "dm_state.h"
+#include "m_random.h"
+#include "p_local.h"
+#include "p_mobj.h"
+#include "p_navigate.h"
+#include "r_misc.h"
+
+
+// TODO
+
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab
---
+++ source_files/edge/p_navigate.h
@@ -0,0 +1,30 @@
+//----------------------------------------------------------------------------
+//  EDGE Navigation System
+//----------------------------------------------------------------------------
+//
+//  Copyright (c) 2022  The EDGE Team.
+//
+//  This program is free software; you can redistribute it and/or
+//  modify it under the terms of the GNU General Public License
+//  as published by the Free Software Foundation; either version 2
+//  of the License, or (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//----------------------------------------------------------------------------
+
+#ifndef __P_NAVIGATE_H__
+#define __P_NAVIGATE_H__
+
+#include "types.h"
+#include "m_math.h"
+
+// TODO
+
+#endif  /*__P_NAVIGATE_H__*/
+
+//--- editor settings ---
+// vi:ts=4:sw=4:noexpandtab

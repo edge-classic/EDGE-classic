157dd4c02b83012e5c3338f104f3d20fc518ab24
Andrew Apted <ajapted@gmail.com>
2022-10-22 16:13:18 +1100

    bot : create `nav_area_c` areas, connected by `nav_link_c` links.

--- source_files/edge/p_navigate.cc
+++ source_files/edge/p_navigate.cc
@@ -19,6 +19,7 @@
 #include "i_defs.h"
 
 #include "con_main.h"
+#include "dm_data.h"
 #include "dm_defs.h"
 #include "dm_state.h"
 #include "m_random.h"
@@ -28,6 +29,7 @@
 #include "p_navigate.h"
 #include "r_defs.h"
 #include "r_misc.h"
+#include "r_state.h"
 
 // DDF
 #include "main.h"
@@ -143,6 +145,91 @@ position_c NAV_NextRoamPoint(bot_t *bot)
 	return position_c { item.x, item.y, item.z };
 }
 
+//----------------------------------------------------------------------------
+
+class nav_area_c
+{
+public:
+	int id         = 0;
+	int first_link = 0;
+	int num_links  = 0;
+
+	nav_area_c(int _id) : id(_id)
+	{ }
+
+	~nav_area_c()
+	{ }
+};
+
+
+class nav_link_c
+{
+public:
+	nav_area_c * dest = NULL;
+
+	bool blocked = false;
+};
+
+
+// there is a one-to-one correspondence from a subsector_t to a
+// nav_area_c in this vector.
+static std::vector<nav_area_c> nav_areas;
+static std::vector<nav_link_c> nav_links;
+
+
+static nav_area_c * NavArea(const subsector_t *sub)
+{
+	if (sub == NULL)
+		return NULL;
+
+	int id = (int)(sub - subsectors);
+
+	SYS_ASSERT(id >= 0 && id < numsubsectors);
+
+	return &nav_areas[id];
+}
+
+
+static void NAV_CreateLinks()
+{
+	for (int i = 0 ; i < numsubsectors ; i++)
+	{
+		nav_areas.push_back(nav_area_c(i));
+	}
+
+	for (int i = 0 ; i < numsubsectors ; i++)
+	{
+		const subsector_t& sub = subsectors[i];
+
+		nav_area_c &area = nav_areas[i];
+		area.first_link = (int)nav_links.size();
+
+		for (const seg_t *seg = sub.segs ; seg != NULL ; seg=seg->sub_next)
+		{
+			nav_area_c *dest = NavArea(seg->back_sub);
+
+			// no link for a one-sided wall
+			if (dest == NULL)
+				continue;
+
+			// ignore player-blocking lines
+			if (! seg->miniseg)
+				if (0 != (seg->linedef->flags & (MLF_Blocking | MLF_BlockPlayers)))
+					continue;
+
+			// NOTE: a big height difference is allowed here, it is checked
+			//       during play (and we need to allow lowering floors etc).
+
+			// WISH: check if link is not blocked by obstacles
+
+			nav_links.push_back(nav_link_c { dest, false });
+			area.num_links += 1;
+
+			//DEBUG
+			// fprintf(stderr, "link area %d --> %d\n", area.id, dest->id);
+		}
+	}
+}
 
 //----------------------------------------------------------------------------
 
@@ -151,12 +238,15 @@ void NAV_AnalyseLevel()
 	NAV_FreeLevel();
 
 	NAV_CollectBigItems();
+	NAV_CreateLinks();
 }
 
 
 void NAV_FreeLevel()
 {
 	big_items.clear();
+	nav_areas.clear();
+	nav_links.clear();
 }
 
 

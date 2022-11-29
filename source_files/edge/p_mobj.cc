98e8dc1bfb3e35880c32e3b5881315ec53e49321
dashodanger <none@none.com>
2022-11-29 16:22:30 -0700

    Thinker culling adjustment

--- source_files/edge/p_mobj.cc
+++ source_files/edge/p_mobj.cc
@@ -1713,8 +1713,7 @@ void P_RunMobjThinkers(void)
 		}
 
 		// Culling test
-		if (r_culling.d == 0 ||
-			R_PointToDist(players[consoleplayer]->mo->x, players[consoleplayer]->mo->y, mo->x, mo->y) < 4000)
+		if (r_culling.d == 0 || gametic % (1 + I_ROUND(R_PointToDist(players[consoleplayer]->mo->x, players[consoleplayer]->mo->y, mo->x, mo->y) / 1000)) == 0)
 			P_MobjThinker(mo);
 	}
 }

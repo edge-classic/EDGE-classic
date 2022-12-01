c297f8671ac4a64af170f1b8662ca84ca4653aa2
dashodanger <none@none.com>
2022-12-01 12:57:03 -0700

    Another try with the flags

--- source_files/dehacked/deh_things.cc
+++ source_files/dehacked/deh_things.cc
@@ -733,7 +733,7 @@ namespace Things
 {
 	typedef struct
 	{
-		long long flag;          // flag in mobjinfo_t (MF_XXX), 0 if ignored
+		int64_t flag;          // flag in mobjinfo_t (MF_XXX), 0 if ignored
 		const char *bex;   // name in a DEHACKED or BEX file
 		const char *conv;  // edge name, NULL if none, can be multiple
 	}

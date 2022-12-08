4307641970be5a66e192a32a8f39cf5067d4c5a7
dashodanger <none@none.com>
2022-12-08 12:58:06 -0700

    Why does MSVC let me compile this stuff

--- source_files/fluidlite/src/fluid_synth.c
+++ source_files/fluidlite/src/fluid_synth.c
@@ -106,13 +106,13 @@ void fluid_synth_settings(fluid_settings_t* settings)
   fluid_settings_register_str(settings, "synth.verbose", "no", 0, NULL, NULL);
   fluid_settings_register_str(settings, "synth.dump", "no", 0, NULL, NULL);
   fluid_settings_register_str(settings, "synth.reverb.active", "yes", 0, NULL, NULL);
-  fluid_settings_register_num(settings, "synth.reverb.room-size", 0.61f, 0.0f, 1.0f, 0, 0, NULL, NULL);
-  fluid_settings_register_num(settings, "synth.reverb.damp", 0.23f, 0.0f, 1.0f, 0, 0, NULL, NULL);
-  fluid_settings_register_num(settings, "synth.reverb.width", 0.76f, 0.0f, 100.0f, 0, 0, NULL, NULL);
-  fluid_settings_register_num(settings, "synth.reverb.level", 0.57, 0.0f, 1.0f, 0, 0, NULL, NULL);
+  fluid_settings_register_num(settings, "synth.reverb.room-size", 0.61f, 0.0f, 1.0f, 0, NULL, NULL);
+  fluid_settings_register_num(settings, "synth.reverb.damp", 0.23f, 0.0f, 1.0f, 0, NULL, NULL);
+  fluid_settings_register_num(settings, "synth.reverb.width", 0.76f, 0.0f, 100.0f, 0, NULL, NULL);
+  fluid_settings_register_num(settings, "synth.reverb.level", 0.57, 0.0f, 1.0f, 0, NULL, NULL);
   fluid_settings_register_str(settings, "synth.chorus.active", "yes", 0, NULL, NULL);
   fluid_settings_register_int(settings, "synth.chorus.nr", FLUID_CHORUS_DEFAULT_N, 0, 99, 0, 0, NULL, NULL);
-  fluid_settings_register_num(settings, "synth.chorus.level", 1.2f, 0.0f, 10.0f, 0, 0, NULL, NULL);
+  fluid_settings_register_num(settings, "synth.chorus.level", 1.2f, 0.0f, 10.0f, 0, NULL, NULL);
   fluid_settings_register_num(settings, "synth.chorus.speed", FLUID_CHORUS_DEFAULT_SPEED, 0.1f, 5.0f, 0, 0, NULL, NULL);
   fluid_settings_register_num(settings, "synth.chorus.depth", FLUID_CHORUS_DEFAULT_DEPTH, 0.0f, 256.0f, 0, 0, NULL, NULL);
   fluid_settings_register_str(settings, "synth.ladspa.active", "no", 0, NULL, NULL);

3194ab14be6c0d3578d5882e12c92c41ef319bba
dashodanger <none@none.com>
2023-04-04 12:00:57 -0600

    Tweak to the last tweak

--- source_files/epi/epi.h
+++ source_files/epi/epi.h
@@ -121,14 +121,8 @@ void I_Debugf(const char *message,...) GCCATTR((format(printf, 1, 2)));
 
 
 // assertion macro
-#ifdef NDEBUG
 #define SYS_ASSERT(cond)  \
 	((cond) ? (void)0 : I_Error("Assertion '%s' failed (%s:%d).\n", #cond , __FILE__ , __LINE__ ))
-#else
-#define SYS_ASSERT(cond)  \
-	((cond) ? (void)0 : I_Warning("Assertion '%s' failed (%s:%d).\n", #cond , __FILE__ , __LINE__ ))
-#endif // NDEBUG
-
 
 //
 // Z_Clear

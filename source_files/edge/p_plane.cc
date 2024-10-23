07dc85c1ce2acba9ffb3a791a447dc0412e10e95
dashodanger <none@none.com>
2024-10-23 11:44:30 -0600

    Fix sector interpolation when removing a plane mover

--- source_files/edge/p_plane.cc
+++ source_files/edge/p_plane.cc
@@ -1585,6 +1585,9 @@ void RunActivePlanes(void)
             if (!pmov->is_ceiling)
                 pmov->sector->floor_move = nullptr;
 
+            pmov->sector->old_ceiling_height = pmov->sector->ceiling_height;
+            pmov->sector->old_floor_height   = pmov->sector->floor_height;
+
             *PMI = nullptr;
             delete pmov;
 

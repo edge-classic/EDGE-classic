52f1bf72a29837773c96f4caa0874a8eef21e836
dashodanger <>
2025-04-06 22:26:43 -0600

    Remove some Windows-related miniaudio defines

--- libraries/miniaudio/miniaudio.cc
+++ libraries/miniaudio/miniaudio.cc
@@ -1,8 +1,6 @@
 #define MA_NO_ENCODING
 #define MA_NO_GENERATION
 #define MA_NO_RESOURCE_MANAGER
-#define MA_NO_WINMM
-#define MA_NO_DSOUND
 // We use our own custom minivorbis decoder
 #define MA_NO_VORBIS
 #define MINIAUDIO_IMPLEMENTATION

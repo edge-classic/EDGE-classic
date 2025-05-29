7e9d35a3516d49082d650a9c903a1f2ac7d62eac
dashodanger <>
2025-05-28 23:24:22 -0600

    Remove hardcoded paths from Linux launch script/desktop file

--- source_files/edge/edge-classic.sh
+++ source_files/edge/edge-classic.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 # Needed to make symlinks/shortcuts work.
 # the binaries must run with correct working directory
-exec "/usr/local/bin/edge-classic.bin" "$@" "-game" "/usr/local/share/edge-classic"
